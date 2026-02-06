1ab179c30c0cb4860a3af41da2db48f159e0a086
Pavithras2001 <pavithrashekar86@gmail.com>
2026-02-06 13:03:20 +0530

    Menu Controller

--- UrbanEats/src/main/java/com/urbanEats/dto/MenuDto.java
+++ UrbanEats/src/main/java/com/urbanEats/dto/MenuDto.java
@@ -3,14 +3,21 @@ package com.urbanEats.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
 @Data
 public class MenuDto {
-	@NotBlank
-	private String name;
-	@NotNull
-	private Double price;
-	@NotBlank
-	private String foodType;
-	@NotBlank
-	private String img;
+
+    private Long id;   
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private Double price;
+
+    @NotBlank
+    private String foodType;
+
+    @NotBlank
+    private String img;
 }
