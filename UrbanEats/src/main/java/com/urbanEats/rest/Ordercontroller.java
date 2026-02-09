56fe3b190d55cd4217015ff041aed77e44b59ee8
Pavithras2001 <pavithrashekar86@gmail.com>
2026-02-09 15:49:36 +0530

    order controller

--- UrbanEats/src/main/java/com/urbanEats/rest/Ordercontroller.java
+++ UrbanEats/src/main/java/com/urbanEats/rest/Ordercontroller.java
@@ -1,11 +1,11 @@
-package com.urbanEats.controller;
+package com.urbanEats.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.urbanEats.dto.request.PlaceOrderRequestDto;
+
 import com.urbanEats.entity.Order;
 import com.urbanEats.service.OrderService;
 
